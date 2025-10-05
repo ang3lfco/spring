4f5fceefe10dc3481e4a57cfbfd0210f627133b4
ang3lfco <ang3lfco@outlook.com>
2025-10-05 00:56:46 -0700

    pasar datos a la vista

--- springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploController.java
+++ springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploController.java
@@ -1,6 +1,7 @@
 package com.ang3lfco.springboot.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-    public String info(){
+    public String info(Model model){
+        model.addAttribute("Titulo", "Servidor en linea");
+        model.addAttribute("Servidor", "Informaticonfig");
+        model.addAttribute("Ip", "192.168.1.1");
         return "detalles_info";
     }
 }
