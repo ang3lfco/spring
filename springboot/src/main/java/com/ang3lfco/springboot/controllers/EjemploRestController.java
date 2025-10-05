a3304f1739f6c462cf1caf7dc02e2677b3446098
ang3lfco <ang3lfco@outlook.com>
2025-10-05 01:17:03 -0700

    creacion de un rest controller

---
+++ springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploRestController.java
@@ -0,0 +1,21 @@
+package com.ang3lfco.springboot.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class EjemploRestController {
+
+    @GetMapping("/detalles_info2")
+    public Map<String, Object> detalles_info2() {
+        Map<String, Object> respuesta = new HashMap<>();
+        respuesta.put("Titulo", "Servidor en linea");
+        respuesta.put("Servidor", "Informaticonfig");
+        respuesta.put("Ip", "192.168.1.1");
+        return respuesta;
+    }
+}
