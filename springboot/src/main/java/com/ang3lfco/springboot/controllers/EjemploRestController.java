a699414a0004966d89cd0781fcd76416dbc2b405
ang3lfco <ang3lfco@outlook.com>
2025-10-05 21:55:44 -0700

    request mapping

--- springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploRestController.java
+++ springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploRestController.java
@@ -5,9 +5,15 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
+@RequestMapping("/api")
+
 public class EjemploRestController {
 
     @GetMapping("/detalles_info2")
