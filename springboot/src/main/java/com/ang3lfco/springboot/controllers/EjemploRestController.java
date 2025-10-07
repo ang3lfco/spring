b3cb96500894a09640c84a4705abbb07dc556eca
ang3lfco <ang3lfco@outlook.com>
2025-10-06 19:18:35 -0700

    Clases DTO

--- springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploRestController.java
+++ springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ang3lfco.springboot.models.Empleados;
+import com.ang3lfco.springboot.models.dto.ClaseDTO;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,19 +19,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EjemploRestController {
 
     @GetMapping("/detalles_info2")
-    public Map<String, Object> detalles_info2() {
-        Empleados empleado1 = new Empleados(
-            "Juan",
-            "Rodriguez",
-            "Calle 1 No 2",
-            "Gerente",
-            35,
-            1234567890,
-            001
-        );
+    public ClaseDTO detalles_info2() {
+        ClaseDTO usuario1 = new ClaseDTO();
+        usuario1.setTitulo("Administrador");
+        usuario1.setUsuario("Informaticonfig");
 
-        Map<String, Object> respuesta = new HashMap<>();
-        respuesta.put("Empleado", empleado1);
-        return respuesta;
+        // Empleados empleado1 = new Empleados(
+        //     "Juan",
+        //     "Rodriguez",
+        //     "Calle 1 No 2",
+        //     "Gerente",
+        //     35,
+        //     1234567890,
+        //     001
+        // );
+
+        // Map<String, Object> respuesta = new HashMap<>();
+        // respuesta.put("Empleado", empleado1);
+        return usuario1;
     }
 }
---
+++ springboot/src/main/java/com/ang3lfco/springboot/models/dto/ClaseDTO.java
@@ -0,0 +1,19 @@
+package com.ang3lfco.springboot.models.dto;
+
+public class ClaseDTO {
+    private String titulo;
+    private String usuario;
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
