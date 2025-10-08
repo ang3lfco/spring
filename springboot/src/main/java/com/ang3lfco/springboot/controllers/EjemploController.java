4d4599dc57149f910fcd531867773b205a6360a5
ang3lfco <ang3lfco@outlook.com>
2025-10-08 14:18:08 -0700

    Model Attribute

--- springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploController.java
+++ springboot/src/main/java/com/ang3lfco/springboot/controllers/EjemploController.java
@@ -1,8 +1,12 @@
 package com.ang3lfco.springboot.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.ang3lfco.springboot.models.Empleados;
 
@@ -23,5 +27,56 @@ public class EjemploController {
         );
         model.addAttribute("Empleado", empleado1);
         return "detalles_info";
+    }
+
+    @ModelAttribute("Empleados")
+    public List<Empleados> listaEmpleados(){
+        return Arrays.asList(
+            new Empleados(
+                "Maria", 
+                "Perez", 
+                "Calle Segunda #2", 
+                "Secretaria", 
+                20, 
+                1234567890, 
+                02
+            ),
+            new Empleados(
+                "Juan", 
+                "Rodriguez", 
+                "Calle primera no.1", 
+                "Gerente", 
+                35, 
+                123456789, 
+                01
+            ),
+            new Empleados(
+                "Luis", 
+                "Martínez", 
+                "Calle tercera no.3", 
+                "Contador", 
+                28, 
+                987654321, 
+                03
+            ),
+            new Empleados(
+                "Ana", 
+                "González", 
+                "Avenida central no.4", 
+                "Diseñadora", 
+                30, 
+                456789123, 
+                04
+            ),
+            new Empleados(
+                "Pedro", 
+                "Sánchez", 
+                "Boulevard no.5", 
+                "Analista", 
+                25, 
+                789123456, 
+                05
+            )
+        );
     }
 }
