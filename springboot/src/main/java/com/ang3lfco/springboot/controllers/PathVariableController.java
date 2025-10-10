ae38fa99793923c3cb803dc0cd3f108b69db5a59
ang3lfco <ang3lfco@outlook.com>
2025-10-10 00:24:53 -0700

    enviar solicitudes post con postman

--- springboot/src/main/java/com/ang3lfco/springboot/controllers/PathVariableController.java
+++ springboot/src/main/java/com/ang3lfco/springboot/controllers/PathVariableController.java
@@ -2,9 +2,15 @@ package com.ang3lfco.springboot.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ang3lfco.springboot.models.Empleados;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -17,4 +23,10 @@ public class PathVariableController {
         parametro1.setInformacion(mensaje);
         return parametro1;
     }
+
+    @PostMapping("/solicitud")
+    public Empleados creaEmple(@RequestBody Empleados empleado1){
+        return empleado1;
+    }
+
 }
